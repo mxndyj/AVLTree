58b33010316994de5e01846e153135b7ae6ee95d
Mandy Jiang <137333628+mxndyj@users.noreply.github.com>
2024-03-26 00:43:59 -0700

    node structure

--- AVL.java
+++ AVL.java
@@ -1,1 +1,22 @@
-hello
+public class AVL {
+    
+    public class Node {
+        private int value;
+        private Node left;
+        private Node right;
+        private int height;
+
+        public Node(int value) {
+            this.value = value;
+            this.height = 0; // assumed leaf node started with height 0
+        
+        }
+    }
+
+    private Node root;
+
+    public AVL() {
+        root = null; //initiated the root to be empty
+    }
+
+}
